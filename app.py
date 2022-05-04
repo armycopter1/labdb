23b28f6df2325fb7ae13cf47c9b4b02fb4306f07
armycopter1 <armycopter@gmail.com>
2022-05-04 13:21:19 -0400

    dertr

--- app.py
+++ app.py
@@ -23,7 +23,13 @@ Session(app)
 
 # Configure CS50 Library to use SQLite database
 # db = SQL("sqlite:///lab.db")
-database_url = os.environ.get('DATABASE_LOC')
+# Use this for pushing to Heroku
+#database_url = os.environ.get('DATABASE_LOC')
+
+# Use this for local
+database_url = os.environ.get('DATABASE_LOC', 'postgresql://')
+
+# Use this for both local and Heroku
 db = SQL(database_url)
 # db.execute("PRAGMA foreign_keys = ON")
 
